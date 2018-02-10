fe3f79f7c0eadfad4568449a92fa21895c8ae38f
Ratnam Yadav <ratnam.yadav@icloud.com>
2018-02-10 20:06:25 +0530

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  telegram_webhooks TelegramWebhooksController
-
+  #telegram_webhooks TelegramWebhooksController
+  post '/webhooks/telegram_vbc43edbf1614a075954dvd4bfab34l1' => 'telegram_webhooks#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
